a8fd1978ba55ace9512ea78b9f6dbf304b6e9dc0
Luke Willis <backspin3354@proton.me>
2024-09-26 17:40:27 -0400

    Hello, world!
    I've setup a basic rust package with `cargo init` and a nix flake using flake-parts, crane and rust-overlay.

---
+++ flake.nix
@@ -0,0 +1,66 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
+    parts.url = "github:hercules-ci/flake-parts";
+    parts.inputs.nixpkgs-lib.follows = "nixpkgs";
+
+    crane.url = "github:ipetkov/crane";
+    
+    rust.url = "github:oxalica/rust-overlay";
+    rust.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs @ {
+    nixpkgs,
+    parts,
+    crane,
+    rust,
+    ...
+  }:
+  parts.lib.mkFlake { inherit inputs; } {
+    systems = [
+      "x86_64-linux"
+    ];
+
+    perSystem = {
+      system,
+      ...
+    }: let 
+      pkgs = nixpkgs.legacyPackages.${system}.extend rust.overlays.default;
+
+      toolchain = pkgs.rust-bin.stable.latest.minimal;      
+      craneLib = (crane.mkLib pkgs).overrideToolchain toolchain;
+
+      devToolchain = pkgs.rust-bin.stable.latest.default.override {
+        extensions = [ "rust-src" "rust-analyzer" ];
+      };
+      devCraneLib = (crane.mkLib pkgs).overrideToolchain devToolchain;
+    
+      craneArgs = {
+        src = craneLib.cleanCargoSource ./.;
+        strictDeps = true;
+
+        nativeBuildInputs = [
+          # TODO
+        ];
+        
+        buildInputs = [
+          # TODO
+        ];
+      };
+
+      package = craneLib.buildPackage (craneArgs // {
+        cargoArtifacts = craneLib.buildDepsOnly craneArgs;
+      });
+    in {
+      packages.default = package;
+      
+      devShells.default = devCraneLib.devShell {
+        # TODO
+      };
+    };
+  };
+}
